6c1ab1a2a1edba3eca431e6bca5fd0654978e32a
shseok <gus0674@ajou.ac.kr>
2022-06-11 19:59:49 +0900

    Set up tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <h1 className='font-bold text-9xl'>hello2</h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
